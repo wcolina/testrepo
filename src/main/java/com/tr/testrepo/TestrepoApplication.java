98498012a84781b9610332721e71055db62aabe3
wcolina <walker.cbdiv@gmail.com>
2018-04-24 02:00:50 -0400

    first commit

---
+++ src/main/java/com/tr/testrepo/TestrepoApplication.java
@@ -0,0 +1,12 @@
+package com.tr.testrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestrepoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tr/testrepo/TestrepoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tr.testrepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestrepoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
